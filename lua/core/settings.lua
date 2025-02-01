982f4630407bcad758ce0e18dd53719898f4f74b
Nagato Yuzuru <nagato@yuzuru.me>
2025-02-01 21:31:30 +0800

    add indentation config

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -6,6 +6,11 @@ vim.opt.mouse = ""
 vim.opt.cursorline = true
 vim.opt.number = true
 
+vim.o.expandtab = true     -- 将 Tab 键转换为空格
+vim.o.shiftwidth = 4       -- 设置缩进宽度为 4
+vim.o.softtabstop = 4      -- 设置 Tab 键行为为 4 个空格
+vim.o.tabstop = 4          -- 设置显示 Tab 的宽度为 4
+
 vim.cmd [[
   highlight LineNr guifg=#dddddd
   highlight CursorLineNr guifg=#ff996c
