02a94db8ae566a395ecc5b4a45d1e627a6678f17
Nagato Yuzuru <nagato@yuzuru.me>
2025-04-07 11:58:00 +0800

    Add .gitignore and update init.lua with new plugins and key mappings

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -11,6 +11,9 @@ vim.o.expandtab = true     -- 将 Tab 键转换为空格
 vim.o.shiftwidth = 4       -- 设置缩进宽度为 4
 vim.o.softtabstop = 4      -- 设置 Tab 键行为为 4 个空格
 vim.o.tabstop = 4          -- 设置显示 Tab 的宽度为 4
+vim.o.clipboard=
+-- vim.opt.clipboard = "unnamedplus"
+
 
 vim.cmd [[
   highlight LineNr guifg=#dddddd
