01ef5f8a06e0e6d3a055a5f60973d412e20cd6b9
Nagato Yuzuru <nagato@yuzuru.me>
2025-04-16 09:01:22 +0800

    fix ruff

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,31 +1,33 @@
 -- ~/.config/nvim/lua/plugins/none-ls.lua
 return function()
     local null_ls = require("null-ls")
+    local builtins = null_ls.builtins
 
     -- 尝试加载 none-ls-extras，用于对 ruff 的支持
-    local extras_ok, extras = pcall(require, "none-ls-extras")
-    if extras_ok then
-        extras.setup()
-    else
-        vim.notify("none-ls-extras 加载失败，请检查安装状态", vim.log.levels.WARN)
-    end
+    -- local extras_ok, extras = pcall(require, "none-ls-extras")
+    -- if extras_ok then
+    --     extras.setup()
+    -- else
+    --     vim.notify("none-ls-extras 加载失败，请检查安装状态", vim.log.levels.WARN)
+    -- end
 
     -- 定义 sources 列表，注意有可能 extras 的模块加载失败时返回 nil
     local sources = {
         -- Lua 格式化使用 stylua
-        null_ls.builtins.formatting.stylua,
+        builtins.formatting.stylua,
+
         require("none-ls.diagnostics.cpplint"),
         require("none-ls.formatting.jq"),
         require("none-ls.code_actions.eslint"),
         -- Python
-        extras_ok and extras.code_actions and extras.code_actions.ruff or nil,
-        extras_ok and extras.diagnostics and extras.diagnostics.ruff or nil,
-
+        -- extras_ok and extras.code_actions and extras.code_actions.ruff or nil,
+        -- extras_ok and extras.diagnostics and extras.diagnostics.ruff or nil,
+        null_ls.builtins.diagnostics.mypy,
         -- Shell 格式化和诊断
         -- null_ls.builtins.formatting.shfmt.with({
         --     -- 如果需要自定义 shfmt 的参数，可添加 extra_args
         --     extra_args = { "--indent=4", "--case-indent" },
-        -- }),
+        null_ls.builtins.diagnostics.zsh, -- }),
         -- null_ls.builtins.diagnostics.shellcheck.with({
         --     filetypes = {
         --         "sh",
@@ -50,7 +52,7 @@ return function()
     end
 
     null_ls.setup({
-        debug = false,
+        debug = true,
         sources = filtered_sources,
     })
 
