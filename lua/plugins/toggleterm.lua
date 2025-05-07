6a417862f26317ad831b17b5984c7e759896d8a4
Nagato Yuzuru <nagato@yuzuru.me>
2025-05-08 06:18:40 +0800

    toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,9 @@
+local term = require("toggleterm")
+
+term.setup({
+	open_mapping = [[<C-`>]],
+	size = 17,
+	start_in_insert = true,
+	shell = vim.o.shell, -- 使用默认 shell
+    hide_numbers = true,  -- 不显示行号          
+})
