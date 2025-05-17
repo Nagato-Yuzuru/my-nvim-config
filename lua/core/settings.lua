08ec1a7c537cc2281d926e9954b82cd2e79e42da
Nagato Yuzuru <nagato@yuzuru.me>
2025-05-18 02:38:07 +0800

    enable mouse

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -2,20 +2,18 @@
 
 vim.opt.langmenu = "en_US.UTF-8"
 vim.cmd("language messages en_US.UTF-8")
-vim.opt.mouse = ""
+vim.opt.mouse = "nvchr"
 vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.termguicolors = true
 
-vim.o.expandtab = true     -- 将 Tab 键转换为空格
-vim.o.shiftwidth = 4       -- 设置缩进宽度为 4
-vim.o.softtabstop = 4      -- 设置 Tab 键行为为 4 个空格
-vim.o.tabstop = 4          -- 设置显示 Tab 的宽度为 4
-vim.o.clipboard=
--- vim.opt.clipboard = "unnamedplus"
-
-
-vim.cmd [[
+vim.o.expandtab = true -- 将 Tab 键转换为空格
+vim.o.shiftwidth = 4 -- 设置缩进宽度为 4
+vim.o.softtabstop = 4 -- 设置 Tab 键行为为 4 个空格
+vim.o.tabstop = 4 -- 设置显示 Tab 的宽度为 4
+vim.o.clipboard =
+	-- vim.opt.clipboard = "unnamedplus"
+	vim.cmd([[
   highlight LineNr guifg=#dddddd
   highlight CursorLineNr guifg=#ff996c
-]]
+]])
