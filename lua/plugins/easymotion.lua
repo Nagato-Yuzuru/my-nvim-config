5ab66ab1ef2f7e47b7fc8470f74349d94c5153f7
Nagato Yuzuru <nagato@yuzuru.me>
2025-06-30 11:08:38 +0800

    update

--- lua/plugins/easymotion.lua
+++ lua/plugins/easymotion.lua
@@ -1,14 +1,19 @@
 -- ~/.config/nvim/lua/plugins/easymotion.lua
 local map = vim.keymap.set
 -- 方向键映射
-map("n", "<Leader><Leader>h", "<Plug>(easymotion-linebackward)")
-map("n", "<Leader><Leader>l", "<Plug>(easymotion-lineforward)")
-map("n", "<Leader><Leader>j", "<Plug>(easymotion-j)")
-map("n", "<Leader><Leader>k", "<Plug>(easymotion-k)")
+mod = { "n", "o", "x" }
+map(mod, "<Leader><Leader>h", "<Plug>(easymotion-linebackward)")
+map(mod, "<Leader><Leader>l", "<Plug>(easymotion-lineforward)")
+map(mod, "<Leader><Leader>j", "<Plug>(easymotion-j)")
+map(mod, "<Leader><Leader>k", "<Plug>(easymotion-k)")
 
--- 保持列位置（不要跳到行首）
+-- 保asdada asd持列位置（不要跳到行首）
+vim.g.EasyMotion_move_highlight = 0
+vim.g.EasyMotion_do_mapping = 0
+vim.g.EasyMotion_smartcase = 1
+
 vim.g.EasyMotion_startofline = 0
 
-map("n", "f", "<Plug>(easymotion-bd-f)", { noremap = false })
-map("n", "t", "<Plug>(easymotion-bd-t)", { noremap = false })
-map("n", "s", "<Plug>(easymotion-s2)", { noremap = false })
+map(mod, "f", "<Plug>(easymotion-bd-f)", { noremap = false })
+map(mod, "t", "<Plug>(easymotion-bd-t)", { noremap = false })
+map(mod, "s", "<Plug>(easymotion-s2)", { noremap = false })
