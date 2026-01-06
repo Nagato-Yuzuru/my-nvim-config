37a5902cac5d4a7fb08024ec88da510a90d1d709
Nagato Yuzuru <nagato@yuzuru.me>
2026-01-06 14:21:16 +0800

    add troubel view; add diff setting

---
+++ lua/plugins/ui/troubel.lua
@@ -0,0 +1,22 @@
+return {
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		position = "bottom", -- 面板位置：bottom, top, left, right
+		height = 10,
+		mode = "workspace_diagnostics", -- 默认模式
+	},
+	keys = {
+		{
+			"<leader>vP",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		-- 4. 查看 LSP 的引用列表 (替代原本的 gr 列表)
+		{
+			"gr",
+			"<cmd>Trouble lsp_references toggle<cr>",
+			desc = "LSP References (Trouble)",
+		},
+	},
+}
