7c314201c8f46e7f327ed276ff75608134581128
Nagato Yuzuru <nagato@yuzuru.me>
2026-03-30 23:32:04 +0800

    add align editor.

---
+++ lua/plugins/edit/align.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"echasnovski/mini.align",
+		version = false,
+		event = "VeryLazy",
+		opts = {},
+		-- 默认键位：
+		--   ga  (Normal/Visual) 交互式对齐（可预览、调整步骤）
+		--   gA  (Normal/Visual) 带预览的对齐
+	},
+}
